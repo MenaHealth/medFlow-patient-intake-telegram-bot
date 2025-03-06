e3fc6002bbe3e2e7a07a116ca0959e3c4121b359
Kyle Essenmacher <kessenmacher7832@gmail.com>
2025-03-06 16:35:02 -0500

    re edit to the telegram bot message

--- utils/languagePrompt.js
+++ utils/languagePrompt.js
@@ -6,14 +6,10 @@ Please select your language by sending the number
 
 الرجاء تحديد اللغة الخاصة بك عن طريق إرسال الرقم
 
-لطفا زبان خود را با ارسال شماره انتخاب کنید
-
-مهرباني وکړئ د شمیرې په لیږلو سره خپله ژبه وټاکئ
 
 1 = English
 2 = العربية
 
-(2 = Arabic / 3 = Farsi / 4 = Pashto)
 `;
 
 // Supported languages
